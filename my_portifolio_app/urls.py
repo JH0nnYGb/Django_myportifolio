dfb80f3e5c227a530b9e4ac11cb90640c96bf022
JH0nnYGb <silvajoneffe@gmail.com>
2025-10-13 21:25:25 -0300

    first commit

---
+++ my_portifolio_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home, name='home_my_profile_django'),
+    path('/projects', views.project, name='my_projects')
+]
